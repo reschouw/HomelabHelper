db25d88d252fab653fc767a06250f0f78f4c545a
reschouw <rybluemip@gmail.com>
2018-03-16 21:28:21 -0700

    Now creating example config file

--- homelabhelper.py
+++ homelabhelper.py
@@ -1,15 +1,31 @@
 # Homelab Helper
 # Ryan Schouweiler
 # A modular homelab helper
-# https://github.com/reschouw/PacketParser
+# https://github.com/reschouw/HomelabHelper
+
 
 import configparser
 
+def createConfig():
+    """Creates example config file"""
+    print('No existing config file. Creating example.')
+    config['test'] = {'Active' : 'yes'}
+    config['test2'] = {'Active' : 'yes'}
+    with open('example.cfg', 'w') as configfile:
+        config.write(configfile)
+    print('Please rename the example.cfg file to helper.cfg after reviewing settings')
+
+    
+
+# Main entry point
 print("Homelab Helper starting...")
 
 config = configparser.ConfigParser()
 
-print(config.read('blah'))
+config.read('helper.cfg')
+if not config.sections():
+    createConfig()
+    exit()
 
 if config['test'].getboolean('Active'):
     print('Test')
