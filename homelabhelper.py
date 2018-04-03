0eb3566c101bb24bb6641e13123da4937ddc9c5b
reschouw <rybluemip@gmail.com>
2018-04-02 22:47:33 -0700

    Refactored slack integration into separate file

--- homelabhelper.py
+++ homelabhelper.py
@@ -7,8 +7,8 @@ import os
 import time
 import re
 import configparser
-from slackclient import SlackClient
-from wakeonlan import send_magic_packet
+
+from slackint import Slack_Bot
 
 
 def createExConfig(config):
@@ -27,60 +27,6 @@ def createExHosts(hosts):
     with open('examplehosts', 'w') as hostsfile:
         hosts.write(hostsfile)
     print('Please rename the \'examplehosts\' file to \'hosts\' after reviewing settings')
-
-def parse_bot_commands(slack_events, require_mention):
-    """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
-    """
-    for event in slack_events:
-        if event['type'] == 'message':
-            if require_mention:
-                user_id, message = parse_direct_mention(event['text'])
-                if user_id == bot_id:
-                    return message, event['channel']
-            else:
-                #Prevent bot responding to it's own messages
-                if 'user' in event and event['user'] != bot_id:
-                    return event['text'], event['channel']
-    return None, None
-            
-
-def parse_direct_mention(message_text):
-    """
-        Finds a direct mention (a mention that is at the beginning) in message text
-        and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
-    matches = re.search("^<@(|[WU].+?)>(.*)", message_text)
-    # the first group contains the username, the second group contains the remaining message
-    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
-
-def handle_command(command, channel):
-    """
-        Executes bot command if the command is known
-    """
-    # Default response is help text for the user
-    default_response = "Not sure what you mean. Try *{}*.".format('help')
-
-    # Finds and executes the given command, filling in response
-    response = None
-    # This is where you start to implement more commands!
-    if command.startswith('help'):
-        response = "List of available commands:\n" + \
-                   "    - help: list available commands"
-    elif command.startswith('wol'):
-        for host in hosts.sections():
-            send_magic_packet(hosts[host]['mac_address'])
-            print('wol:', hosts[host]['mac_address'])
-        response = "Sent!"
-
-    # Sends the response back to the channel
-    slack_client.api_call(
-        "chat.postMessage",
-        channel=channel,
-        text=response or default_response
-    )
 
 # Main entry point
 if __name__ == "__main__":
@@ -119,24 +65,21 @@ if __name__ == "__main__":
         print (err.args)
         exit(1)
             
-    #Connect to Slack and start bot
-    #Credit to Matt Makai @fullstackpython for starter code
+    #Connect to Slack, start, and run bot
     if 'bot_token' in config['Slack Integration']:
-        slack_client = SlackClient(config['Slack Integration']['bot_token'])
+        slack_bot = Slack_Bot(config['Slack Integration']['bot_token'],
+                              require_mention,
+                              hosts)
     else:
-        slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-    bot_id = None
-    if slack_client.rtm_connect(with_team_state=False, auto_reconnect=True):
-        print("Homelab Helper Bot connected and running!")
-        # Read bot's user ID by calling Web API method `auth.test`
-        bot_id = slack_client.api_call("auth.test")["user_id"]
+        slack_bot = Slack_Bot(os.environ.get('SLACK_BOT_TOKEN'),
+                                             require_mention,
+                                             hosts)
+    if slack_bot.connect():
         while True:
-            command, channel = parse_bot_commands(slack_client.rtm_read(), require_mention)
-            if command:
-                handle_command(command, channel)
+            slack_bot.read_command()
             time.sleep(refresh_rate)
     else:
-        print("Connection failed. Exception traceback printed above.")
+        exit(1)
         
         
         
---
+++ slackint.py
@@ -0,0 +1,99 @@
+# Homelab Helper
+# Slack Integration
+# Ryan Schouweiler
+# Credit to Matt Makai @fullstackpython for starter code
+# A modular homelab helper
+# https://github.com/reschouw/HomelabHelper
+
+from slackclient import SlackClient
+from wakeonlan import send_magic_packet
+
+
+
+class Slack_Bot:
+    
+    def __init__(self, bot_token, require_mention, hosts):
+        """
+            Creates slack client with given bot token and settings
+        """
+        self.slack_client = SlackClient(bot_token)
+        self.bot_id = None
+        self.require_mention = require_mention
+        self.hosts = hosts
+        
+            
+    def connect(self):
+        """
+            Connects slack client and gets assigned bot id
+        """
+        if self.slack_client.rtm_connect(with_team_state=False,
+                                         auto_reconnect=True):
+            print("Homelab Helper Bot connected and running!")
+            # Read bot's user ID by calling Web API method `auth.test`
+            self.bot_id = self.slack_client.api_call("auth.test")["user_id"]
+            return True
+        else:
+            print("Connection failed. Exception traceback printed above.")
+            return False
+    
+    def read_command(self):
+        """
+            Checks for new messages on slack and handles them appropriately
+        """
+        command, channel = self.parse_bot_commands(self.slack_client.rtm_read())
+        if command:
+            self.handle_command(command, channel)
+    
+    def parse_bot_commands(self, slack_events):
+        """
+            Parses a list of events coming from the Slack RTM API to find bot commands.
+            If a bot command is found, this function returns a tuple of command and channel.
+            If its not found, then this function returns None, None.
+        """
+        for event in slack_events:
+            if event['type'] == 'message':
+                if self.require_mention:
+                    user_id, message = parse_direct_mention(event['text'])
+                    if user_id == self.bot_id:
+                        return message, event['channel']
+                else:
+                    #Prevent bot responding to it's own messages
+                    if 'user' in event and event['user'] != self.bot_id:
+                        return event['text'], event['channel']
+        return None, None
+
+    def parse_direct_mention(self, message_text):
+        """
+            Finds a direct mention (a mention that is at the beginning) in message text
+            and returns the user ID which was mentioned. If there is no direct mention, returns None
+        """
+        matches = re.search("^<@(|[WU].+?)>(.*)", message_text)
+        # the first group contains the username, the second group contains the remaining message
+        return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+    def handle_command(self, command, channel):
+        """
+            Executes bot command if the command is known
+        """
+        # Default response is help text for the user
+        default_response = "Not sure what you mean. Try *{}*.".format('help')
+
+        # Finds and executes the given command, filling in response
+        response = None
+        if command.startswith('help'):
+            #Print available options or more info about an option
+            response = "List of available commands:\n" + \
+                       "    - help: list available commands"
+        elif command.startswith('wol'):
+            #
+            for host in self.hosts.sections():
+                send_magic_packet(self.hosts[host]['mac_address'])
+                print('wol:', self.hosts[host]['mac_address'])
+            response = "Sent!"
+
+        # Sends the response back to the channel
+        self.slack_client.api_call(
+            "chat.postMessage",
+            channel=channel,
+            text=response or default_response
+        )
