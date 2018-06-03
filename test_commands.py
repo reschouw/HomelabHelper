8866c3bbf6512fbed8bb1265e865f5e90cc24090
reschouw <rybluemip@gmail.com>
2018-06-03 10:30:37 -0700

    Adding travis-ci integration

---
+++ test_commands.py
@@ -0,0 +1,8 @@
+# Homelab Helper
+# Testing module for commands
+# Ryan Schouweiler
+# A modular homelab helper
+# https://github.com/reschouw/HomelabHelper
+
+def test_example():
+    print "Hello World!"
