0d9eb618b3e2daa822df7023363462f81a0394a8
reschouw <rybluemip@gmail.com>
2018-06-02 18:18:31 -0700

    added licence and intro text on startup

--- homelabhelper.py
+++ homelabhelper.py
@@ -13,6 +13,10 @@ from confreader import *
 # Main entry point
 if __name__ == "__main__":
     print("Homelab Helper starting...")
+    
+    print("Copyright (C) 2018  Ryan Schouweiler")
+    
+    print("This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.")
 
     #Open and verify config files
     config, hosts = openConfigs()
