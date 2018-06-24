2d33a6b9867aacb2aa9356cf36961baa04f8f6f9
Ryan Schouweiler <reschouw@users.noreply.github.com>
2018-06-24 15:12:21 -0700

    added info tests

--- tests/test_info.py
+++ tests/test_info.py
@@ -12,20 +12,31 @@ from unittest.mock import patch
 import configparser
 
 #Modules to be tested
-from commands import wol
+from commands import info
 
 
 
 class test_wol(unittest.TestCase):
-
+    
+    hosts = configparser.ConfigParser()
+    hosts['host1'] = {'wol_ready' : True,  'mac_address' : '11:11:11:11:11:11'}
+    hosts['host2'] = {'wol_ready' : True,  'mac_address' : '22:22:22:22:22:22'}
+    hosts['host3'] = {'wol_ready' : False, 'mac_address' : '33:33:33:33:33:33'}
+    hosts['host4'] = {'wol_ready' : True,  'mac_address' : '44:44:44:44:44:44'}
+    
     def test_info_none(self):
-        pass
+        response = info("info", self.hosts)
+        self.assertIs(type(response), str)
     
     def test_info_ip(self):
-        pass
+        response = info("info ip", self.hosts)
+        self.assertIs(type(response), str)
         
     def test_info_hosts(self):
-        pass
+        response = info("info hosts", self.hosts)
+        self.assertIs(type(response), str)
     
     def test_info_host(self):
-        pass
+        response = info("info host MiniVerse", self.hosts)
+        self.assertIs(type(response), str)
+        response = info("info host host1", self.hosts)
